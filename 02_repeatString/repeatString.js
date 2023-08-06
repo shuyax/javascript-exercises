ceb6357beac3d7f183c0328fb9c66621fdfff3a6
Mac Mini <macmini@mac-mini.myfiosgateway.com>
2023-08-06 14:41:47 -0400

    add the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let strings ='';
+    for (let i = 0; i < num; i++) {
+        strings = strings + string;       
+    } 
+    return strings;
 };
 
 // Do not edit below this line

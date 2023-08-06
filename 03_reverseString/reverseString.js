f829e36ec3e55ac676f77261f0316a81b6d731d1
Mac Mini <macmini@mac-mini.myfiosgateway.com>
2023-08-06 15:19:40 -0400

    add reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let output = '';
+    for (let i = 0; i < input.length; i++){
+        output += input.slice(input.length - 1 - i, input.length - i);
 
+    }
+    return output;
 };
 
 // Do not edit below this line

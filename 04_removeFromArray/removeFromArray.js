ae9f426a72fe926eb035659a556ff3caaa0e655c
Mac Mini <macmini@mac-mini.myfiosgateway.com>
2023-08-09 20:02:39 -0400

    add function remove multiple arguments from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
 const removeFromArray = function() {
+    //let array = arguments[0].slice();
+    //console.log(arguments[0]);
+    for (let j = 1; j < arguments.length; j++) {
+        for (let i = 0; i < arguments[0].length; i++) {
+            if (arguments[0][i] === arguments[j]) {
+                console.log(i);
+                console.log(arguments[0][i]);
+                arguments[0].splice(i,1);
+                console.log(arguments[0]); 
+            
+            } 
+            //array.push(arguments[0][i]); 
+        }    
+    }
+
+    //console.log("test");
+    //console.log(array);
+    //console.log("test2");
+    return arguments[0];
 
 };
 

0d1b3663959fa4c466b4ecc0086e8e5bbb8334a2
Mac Mini <macmini@mac-mini.myfiosgateway.com>
2023-08-12 00:08:06 -0400

    add the sumAll function with two arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
 const sumAll = function() {
+    let sum = 0;
+    if (arguments[0] >= 0 && arguments[1] >= 0 && typeof(arguments[0]) === "number" && typeof(arguments[1]) === "number"){
+        if (arguments[1] - arguments[0] >= 0){
+            for (let i = arguments[0]; i <= arguments[1]; i++){
+                sum += i;
+            }
 
+            
+        } else {
+            for (let i = arguments[1]; i <= arguments[0]; i++){
+                sum += i;
+            }
+        }
+        console.log(typeof("90") === "string");
+        return sum;
+    } else {
+        
+        return 'ERROR';
+    }
+    
+    
+    
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;

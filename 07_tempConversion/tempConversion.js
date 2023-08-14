b1405d2ae1abed36032c4b7db2a558f0fe40da06
Mac Mini <macmini@mac-mini.myfiosgateway.com>
2023-08-13 21:10:05 -0400

    add functions that convert c to f and convert f to c

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) * 5/9;
+  c = Math.round(c * 10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = 1.8 * c + 32;
+  f = Math.round(f * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line

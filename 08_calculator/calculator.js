7f89614733fe8248f749c14b7c496cc034fb9ddd
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-02-26 12:03:05 -0500

    add the calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(a, b) {
+  let num = a + b;
+  return num;
+};
+
+const subtract = function(a, b) {
+  let num = a - b;
+  return num;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  const total = array.reduce((total, current_item) => total + current_item, 0)
+  return total
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  const num = array.reduce((total,current) => total * current, 1)
+  return num
+  
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+  const num = a**b
+  return num
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factor = 1
+  for(let j = 1; j <= a; j++) {
+    factor = factor * j;
+  }
+	return factor
 };
 
 // Do not edit below this line

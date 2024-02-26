c0114ba23d08c5153d56c54875a92640073ba00d
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-02-26 17:18:50 -0500

    Use the reduce method to return an object with the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestAge = people.reduce((oldestperson,person) => {
+        if (person.yearOfDeath == undefined){person.yearOfDeath = new Date().getFullYear()}
+        return (person.yearOfDeath - person.yearOfBirth) > (oldestperson.age) 
+        ? {name: person.name, age: person.yearOfDeath - person.yearOfBirth} : oldestperson
+}, {name: '', age: 0})
 
+return oldestAge
 };
 
 // Do not edit below this line

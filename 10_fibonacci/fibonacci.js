eae1fcafa37eb8aeb350004a50177cf653d9fc60
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-02-26 13:54:24 -0500

    initial commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if(index >= 0){
+        let array = [1,1]
+    for (let j = 1; j < index; j++){
+        array[j + 1] = array[j] + array[j-1]
+    }
+    console.log(index, array)
+    return array[index-1]
+    } else {
+        return 'OOPS'
+    }
+    
+
 
 };
 

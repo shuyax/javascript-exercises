05ef96e0eb83d4001d223ba37d148b9863e50179
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-02-26 13:43:33 -0500

    initial commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.split('')
+    for (i = 0; i < string.length; i++){
+        string[i] = string[i].toLowerCase()
+        if (string[i] == ' '){
+            string[i] = ''
+        } else if (string[i] > 'z' || string[i] < 'a'){
+            string[i] = ''
+        }
+    }
+    console.log(string)
+    const temp1 = string.join('')
+    
+    console.log(temp1)
+    
+    const temp = string.reverse().join('')
+    console.log(temp)
+    
+    return temp === temp1
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
